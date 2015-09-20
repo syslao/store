ecfdbb47e6095a109ad71244baead32facb5029d
modems <syslao@gmail.com>
2015-09-20 17:32:56 +0300

    add Item model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   # get 'sessions/new'
 
   # get 'sessions/create'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920133831) do
+ActiveRecord::Schema.define(version: 20150920142906) do
+
+  create_table "items", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
