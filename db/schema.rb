9a6b457dde168c168c561086de9192b947f27c2b
syslao <syslao@gmail.com>
2015-09-23 17:17:12 +0300

    add visible attr  to item

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922144822) do
+ActiveRecord::Schema.define(version: 20150923133717) do
 
   create_table "items", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "visible",             default: false
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
